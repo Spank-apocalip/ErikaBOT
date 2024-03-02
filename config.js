bc4e764522b820258060cef91ebc8831fa3b0fb9
Spank-apocalip <161268583+Spank-apocalip@users.noreply.github.com>
2024-03-02 00:21:41 -0300

    config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ import fs from 'fs'
 import { en, es, id, ar, pt } from './lib/idiomas/total-idiomas.js' 
 //⊱ ━━━━━.⋅ Añada los numeros a ser Propietario/a | Add the numbers to be Owner ⋅.━━━━ ⊰
 global.owner = [
-  ['16479546631','Erika-Bot', true],    
-  ['16474952517']
+  ['559285013050','Erika-Bot', true],    
+  ['559285013050']
 
 ]
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
